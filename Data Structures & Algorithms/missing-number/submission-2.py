599e17e94db3291612af273ba7eda325f87e46bb
Daniyal Khan <d5khan@ucsd.edu>
2026-07-07 22:47:43 -0700

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        for i in range(len(nums)): 
+            if nums[i] != i: 
+                return i 
+        return len(nums)
+        
